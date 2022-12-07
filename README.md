b5d2deafc9e442b583e8492ee1b22ad6cb85f21a
Yuri Kopshev <yura.kopshev@gmail.com>
2022-12-07 16:05:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Домашнее задание к занятию «Espresso» часть 1
+
+
+---
+
+## Задание 1. Реализация тестов на проверку главного экрана
+
+Написаны тесты проверяющие элементы выпадающего списка в MainActivity
+
+Тестовый файл расположен:
+androidTest/java/com/example/myapplication/MainActivityTest.java
+
+## Задание 2. Подготовка отчетов
+
+1. Подготовлен базовый отчет в формате html
+2. Подготовлен отчет при помощи Allure
+   [allure report](https://drive.google.com/file/d/1MsTtK_Llqf6a9l17NBWg5LSvuOyQn1-l/view?usp=share_link)
+   [html report]()
+
+
+
---
+++ app/src/androidTest/java/com/example/myapplication/MainActivityTest.java
@@ -0,0 +1,110 @@
+package com.example.myapplication;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+
+@RunWith(AllureAndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityHomeTest() {
+        ViewInteraction textView = onView(withId(R.id.text_home));
+        textView.check(matches(withText("This is home fragment")));
+    }
+
+    @Test
+    public void mainActivitySlideShowTest() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("com.google.android.material.appbar.AppBarLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+
+        ViewInteraction navigationMenuItemView = onView(withId(R.id.nav_slideshow));
+        navigationMenuItemView.check(matches(isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+
+        ViewInteraction textView = onView(withId(R.id.text_slideshow));
+        textView.check(matches(isDisplayed()));
+        textView.check(matches(withText("This is slideshow fragment")));
+    }
+
+    @Test
+    public void mainActivityGalleryTest() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("com.google.android.material.appbar.AppBarLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(withId(R.id.nav_gallery));
+        navigationMenuItemView.check(matches(isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.text_gallery));
+        textView.check(matches(isDisplayed()));
+        textView.check(matches(withText("This is gallery fragment")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
